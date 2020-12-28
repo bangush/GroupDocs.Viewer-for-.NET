a081c30104bae078fa8b5afaa37ccee091cf0a84
Mikhail Evgrafov <mikhail.evgrafov@aspose.com>
2020-12-28 19:12:06 +0500

    Example "Example demonstrates how to render CHM document into HTML, JPG, PNG, PDF." was updated.

--- Examples/GroupDocs.Viewer.Examples.CSharp/AdvancedUsage/Rendering/RenderingOptionsByDocumentType/RenderingWebDocuments/RenderingChmFiles.cs
+++ Examples/GroupDocs.Viewer.Examples.CSharp/AdvancedUsage/Rendering/RenderingOptionsByDocumentType/RenderingWebDocuments/RenderingChmFiles.cs
@@ -14,34 +14,38 @@ namespace GroupDocs.Viewer.Examples.CSharp.AdvancedUsage.Rendering.RenderingOpti
         public static void Run()
         {
             string outputDirectory = Utils.GetOutputDirectoryPath();
-            string pageFilePathFormat = Path.Combine(outputDirectory, "chm_result.html");
+            string pageFilePathFormat = Path.Combine(outputDirectory, "chm_result_{0}.html");
 
             // TO HTML
             using (Viewer viewer = new Viewer(TestFiles.SAMPLE_CHM))
             {
                 HtmlViewOptions options = HtmlViewOptions.ForEmbeddedResources(pageFilePathFormat);
+                options.RenderSinglePage = true; // Enable it if you want to convert all CHM content to single page
 
-                viewer.View(options);
+                //viewer.View(options,1,2,3); // Convert only 1,2,3 pages
+                 viewer.View(options); // Convert all pages
             }
 
             // TO JPG
-            pageFilePathFormat = Path.Combine(outputDirectory, "chm_result.jpg");
+            pageFilePathFormat = Path.Combine(outputDirectory, "chm_result_{0}.jpg");
 
             using (Viewer viewer = new Viewer(TestFiles.SAMPLE_CHM))
             {
                 JpgViewOptions options = new JpgViewOptions(pageFilePathFormat);
 
-                viewer.View(options);
+                viewer.View(options,1,2,3); // Convert only 1,2,3 pages
+                //viewer.View(options); // Convert all pages
             }
 
             // TO PNG
-            pageFilePathFormat = Path.Combine(outputDirectory, "chm_result.png");
+            pageFilePathFormat = Path.Combine(outputDirectory, "chm_result_{0}.png");
 
             using (Viewer viewer = new Viewer(TestFiles.SAMPLE_CHM))
             {
                 PngViewOptions options = new PngViewOptions(pageFilePathFormat);
 
-                viewer.View(options);
+                viewer.View(options,1,2,3); // Convert only 1,2,3 pages
+                //viewer.View(options); // Convert all pages
             }
 
             // TO PDF
@@ -51,7 +55,7 @@ namespace GroupDocs.Viewer.Examples.CSharp.AdvancedUsage.Rendering.RenderingOpti
             {
                 PdfViewOptions options = new PdfViewOptions(pageFilePathFormat);
 
-                viewer.View(options);
+                viewer.View(options); // Convert all pages
             }
 
             Console.WriteLine($"\nSource document rendered successfully.\nCheck output in {outputDirectory}.");
